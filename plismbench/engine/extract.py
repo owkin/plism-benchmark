9c49133d0ab2837a50e1cf34ebc7620ccb029495
Antoine Olivier <antoine.olivier@owkin.com>
2025-03-03 15:42:13 +0000

    glob and assert in extract

--- plismbench/engine/extract.py
+++ plismbench/engine/extract.py
@@ -195,10 +195,10 @@ def run_extract(
     image_transform = feature_extractor.transform
     device = feature_extractor.device
 
-    slide_h5_paths = list(download_dir.glob("*_to_GMH_S60.tif.h5"))
-    # assert (n_slides := len(slide_h5_paths)) == NUM_SLIDES, (
-    #    f"Download uncomplete: found {n_slides}/{NUM_SLIDES}"
-    # )
+    slide_h5_paths = list(download_dir.glob("*.tif.h5"))
+    assert (n_slides := len(slide_h5_paths)) == NUM_SLIDES, (
+        f"Download uncomplete: found {n_slides}/{NUM_SLIDES}"
+    )
 
     for slide_h5_path in tqdm(slide_h5_paths):
         # Get slide id
